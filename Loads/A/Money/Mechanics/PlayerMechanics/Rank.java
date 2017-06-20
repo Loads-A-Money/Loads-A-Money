1ae2b0505910eca6d0f330adc3cf42ab6412d277
Vinstaal0 <vincent-staaltjens@live.nl>
2017-06-20 10:54:51 +0200

    Added visuality ranking

--- Loads/A/Money/Mechanics/PlayerMechanics/Rank.java
+++ Loads/A/Money/Mechanics/PlayerMechanics/Rank.java
@@ -2,21 +2,22 @@ package Loads.A.Money.Mechanics.Playermechanics;
 
 public Enum Rank {
 	
-	// if something works for a rank, it should also work using all ranks above, via PermissionMechanics
-	OWNER(999, ChatColor.AQUA + "OWNER"),
-	ADMIN(900, ChatColor.PURPLE + "ADMIN"),
-	GAME_MODERATOR(700, ChatColor.AQUA + "GM"),
-	PLAYER_MODERATOR(400, ChatColor.AQUA + "PMOD"),
-	WORLD_DEV(350, ChatColor.GREEN + "WRLD"),
-	BUILDER(300, ChatColor.DARK_GREEN + "BLD"),
-	SUB(100, ChatColor.GOLD + "SUB");
+	OWNER(999, ChatColor.AQUA + "OWNER", 7),
+	ADMIN(900, ChatColor.PURPLE + "ADMIN", 6),
+	GAME_MODERATOR(700, ChatColor.AQUA + "GM", 5),
+	PLAYER_MODERATOR(400, ChatColor.AQUA + "PMOD", 3),
+	WORLD_DEV(350, ChatColor.GREEN + "WRLD", 4),
+	BUILDER(300, ChatColor.DARK_GREEN + "BLD", 2),
+	SUB(100, ChatColor.GOLD + "SUB", 1);
 	
 	private int priority;
 	private String tag;
+	private int visualPrio;
 	
-	public Rank(int priority, String tag) {
+	public Rank(int priority, String tag, int visualPrio) {
 		this.priority = priority;
 		this.tag = tag;
+		this.visualPrio = visualPrio;
 	}
 	
 	protected int getPriority() {
@@ -26,6 +27,10 @@ public Enum Rank {
 	// Runs via PermissonMechanics
 	protected String getTag() {
 		return this.tag;
+	}
+	
+	protected String getVisualprio() {
+		return this.visualPrio;
 	}
 
 }
