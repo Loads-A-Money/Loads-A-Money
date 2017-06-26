ad9c06a63927b93c336a071f939d821ab40f8825
Vinstaal0 <vincent-staaltjens@live.nl>
2017-06-26 16:04:31 +0200

    Update LoadsALobby.java

--- Loads/A/Lobby/LoadsALobby.java
+++ Loads/A/Lobby/LoadsALobby.java
@@ -1,6 +1,6 @@
 package Loads.A.Lobby;
 
-public class LoadsALobby.java extends JavaPlugin {
+public class LoadsALobby extends JavaPlugin implements CommandExecutor {
 
 	@Override
 	public void onEnable() {
@@ -11,5 +11,16 @@ public class LoadsALobby.java extends JavaPlugin {
 	public void onDisable() {
 	
 	}
+	
+	IconMenu menu = new IconMenu("My Fancy Menu", 9, new IconMenu.OptionClickEventHandler() {
+            @Override
+            public void onOptionClick(IconMenu.OptionClickEvent event) {
+                event.getPlayer().sendMessage("You have chosen " + event.getName());
+                event.setWillClose(true);
+            }
+        }, plugin)
+        .setOption(3, new ItemStack(Material.APPLE, 1), "Food", "The food is delicious")
+        .setOption(4, new ItemStack(Material.IRON_SWORD, 1), "Weapon", "Weapons are for awesome people")
+        .setOption(5, new ItemStack(Material.EMERALD, 1), "Money", "Money brings happiness");
 
 }
